e6d6b8f0f397b6ed291db90bd39458ab70ce1b65
Wesley Fisher <wfisher@vespasian.reality.dataforge.network>
2020-10-30 16:32:20 -0500

    fixed some mistakes

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,5 +1,16 @@
-import cron from "node-cron";
+import cron from 'node-cron';
 
-cron.schedule('* * * * *', () => {
-    console.log("Did a thing");
-})
+module.exports = function Task(name,time,type,command,args) {
+    this.name = name;
+    this.time = time;
+    this.type = type;
+    this.command = command;
+    this.args = args
+
+    this.run = function(){
+        cron.schedule(time, () => {
+            console.log("Did a thing");
+        });
+    }
+    
+}
